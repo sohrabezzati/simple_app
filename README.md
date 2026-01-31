281a8a42953ea9e553682d3cf9b5f3e922a2ab88
sohrabezzati <s.ezzati.30.dec.1999@gmail.com>
2026-01-31 12:45:20 +0430

    Refactor code structure for improved readability and maintainability

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ For test
 please use test@example.com as correct email
 and 123456A as correct password
  
+![Login](screenshots/login.png)
+![Wrong Email](screenshots/wrongemail.png)
+![Wrong Password](screenshots/wrongpass.png)
+![Loginning in](screenshots/loginningin.png)
+![Logged in](screenshots/loggedin.png)
--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/constants/styles.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('You have succesfully logged in!')),
+      body: Center(
+        child: Text(
+          'You have succesfully logged in!',
+          style: Styles.instance.textHeadLinLarg(),
+        ),
+      ),
     );
   }
 }
